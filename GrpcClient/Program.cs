e29b5a23fa569cbc7b982a7a5af4aa50d4c7fe85
DinuGarbuz <DinuGarbuz@users.noreply.github.com>
2020-04-28 10:44:09 +0300

    tema2

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {           
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+            Console.WriteLine("Date: ");
+            DateTime input;//= Console.ReadLine();
+            // DateTime dt;
+            
+            var auxx = Console.ReadLine();           
+            Console.WriteLine(auxx);
+            bool isValid = DateTime.TryParseExact(
+           auxx,
+           "MM/dd/yyyy",
+           CultureInfo.InvariantCulture,
+           DateTimeStyles.None,
+           out input);
+        
+            if (isValid)
+            {
+                var clientRequested = new CustomerLookupModel { Date = auxx };
+                var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+            }
+            else
+            {
+                Console.WriteLine("gresit");
+            }    
+
+        
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Globalization;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            DateTime input;
+            var auxx = request.Date;
+            Console.WriteLine(auxx);
+            bool isValid = DateTime.TryParseExact(
+           auxx,
+           "MM/dd/yyyy",
+           CultureInfo.InvariantCulture,
+           DateTimeStyles.None,
+           out input);
+
+            string[] lines = System.IO.File.ReadAllLines("zodie.txt");
+
+
+            DateTime input1;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                bool isValid1 = DateTime.TryParseExact(
+                     lines[i],
+                     "MM/dd/yyyy",
+                         CultureInfo.InvariantCulture,
+                       DateTimeStyles.None,
+                      out input1);
+                
+
+                if (input.DayOfYear <= input1.DayOfYear)
+                {
+                    Console.WriteLine("Zodia: " + lines[i + 1]);
+                    break;
+                }
+
+            }
+            return Task.FromResult(new CustomerModel { });
+
+        }
+    }
+}
